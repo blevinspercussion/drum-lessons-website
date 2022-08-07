57fb50f948b97f66296027726e690ad87c60eb12
blevinspercussion <blevinspercussion@gmail.com>
2022-08-07 09:49:03 -0500

    added content to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,9 @@ import './components.css';
 
 function Header () {
     return (
-        <div>
-
+        <div className='head'>
+            <h1 className='title'>Blevins Percussion</h1>
+            <h3>North Georgia's Premier Spot for Expert Drum Lessons</h3>
         </div>
     )
 }
