44e891199c9b0709155bbd0c0cf8128e006cc85a
Adam Blevins <blevinspercussion@gmail.com>
2022-08-13 17:08:12 -0500

    Added content to intro section, and added more css styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './components.css';
+
+function Footer () {
+    return (
+        <div id='footer'>
+            <p>
+                All text and videos copyright &copy; 2022 Adam Blevins, Blevins Percussion
+            </p>
+            <p>
+                All photos copyright &copy; Ashe Derrick, Ashely B Photography
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,41 @@
 import './components.css';
 
+import purpleDrumlites from './purple-drumlites.jpg';
+
 function Intro () {
     return (
-        <div>
+        <div id='intro'>
+            <div className='text-box'>
+                <h3>
+                    I'm Adam Blevins, the owner and lead instructor at Blevins Percussion.
+                </h3>
+                <p>
+                    In the last 25 years I have worked with hundreds of student both privately, as well as 
+                    with public and private school music programs. I have helped dozens of percussion sections 
+                    and drumlines achieve superior ratings at competitions and festivals, and have helped my 
+                    private students achieve their goals of attaining spots in honor bands, excelling in their 
+                    shool band program, and I have even had some students go on to pursue careers as professional 
+                    musicians and music educators.
+                </p>
 
+                <p>
+                    Not only have a taught others how to perform on drums and percussion, but I have also walked the 
+                    walk as a professional musician, playing gigs all over the southeastern United States and beyond, 
+                    from Florida to Iowa. I have gained a wealth of knowledge from my time as a working, professional 
+                    musician to all of my students, whether their goal is to play in a band with their friends, study 
+                    music at the collegiate level, or work as a touring drummer or percussionist.
+                </p>
+                <p>
+                    While your here, be sure to read about my qualifications in my bio, or why you should study with 
+                    Blevins Percussion. If you would like to inquire about private lessons, book me for a clinic, or 
+                    have me come work with your school percussion section or drumline, please see my contact page for 
+                    contact info.
+                </p>
+            </div>
+
+            <div>
+                <img className='inline-image' src={purpleDrumlites}></img>
+            </div>
         </div>
     )
 }
