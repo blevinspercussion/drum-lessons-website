9cd0c1fe0af0491458b010336d074874b08a7390
Adam Blevins <blevinspercussion@gmail.com>
2022-08-17 09:19:56 -0500

    Added more styling, changed header layout, added content page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,20 @@ import './components.css'
 
 function Contact () {
     return (
-        <div>
-
+        <div className='content'>
+            <div className='text-box'>
+                <h1>Contact</h1>
+                <p>
+                    My home studio is conveniently located in Rossville, GA, off of Battlefield Parkway, just past 
+                    Highway 27. If you would like book a lesson slot, you can call or text me at 706-623-DRUM (706-623-3786) 
+                    or you can reach out via e-mail at blevinspercussion@gmail.com.
+                </p>
+                <p>
+                    I have availability from Monday through Thursday between the hours of 2:00 and 7:00 p.m., Eastern Time, but 
+                    specific open time slots fluctuate. Be sure to get in touch with me as soon as possible to see what times I 
+                    have available!
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,18 @@ import { Link } from 'react-router-dom';
 function Header () {
     return (
         <div className='head'>
+            <div>
             <Link to={'/'}>
                 <img className='logo' src={logo} alt='blevins percussion logo'></img>
             </Link>
-            <div className='vl'></div>
             <h3>North Georgia's Premier Spot for <span className='hilight-text'>Expert</span> Drum Lessons</h3>
+            </div>
+            <div className='vl'></div>
+            <div>
+            
+            <h3>706 623 DRUM</h3>
+            <h3>blevinspercussion@gmail.com</h3>
+            </div>
         </div>
     )
 }
