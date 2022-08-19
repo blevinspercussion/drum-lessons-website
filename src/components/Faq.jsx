7a8ab9744894a93dd400034c686fff69d015ea50
Adam Blevins <blevinspercussion@gmail.com>
2022-08-19 13:05:28 -0500

    Reworked the FAQ page

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,103 +5,43 @@ function Faq () {
         <div className='content faq'>
             <div className='text-box'>
             <div className='question'>
-                <h2>Do I need a private instructor?</h2>
+                <h2>How much do drum lessons cost?</h2>
             </div>
             <div className='answer'>
                 <p>
-                    We live in a truly amazing time in that there is a wealth of information that can be had for 
-                    free or very cheap through websites such as Youtube. Unfortunately, creating content on 
-                    these platforms is almost as easy as consuming it, so nearly anyone can do it. This has resulted 
-                    in a lot of bad information being passed around. And even if you are able to parse through the bad 
-                    information, there is no structure or feedback to the 'lessons'.
-                </p>
-                <p>
-                    By studying with a qualified private instructor, such as at Blevins Percussion, you will receive 
-                    a long-term comprehensive lesson plan designed to help you reach your personal goals as efficiently 
-                    as possible. Not only that, but you will also receive feedback on what you're doing correctly as 
-                    well as what you can improve upon as well as <em>how</em> to improve. 
-                </p> 
-                <p>
-                    Maybe you are in a band program at your high school or middle school. In that kind of situation, your 
-                    band director has a tremendous amount of work on their plate. They are responsible for teaching reading, 
-                    technique, style, etc. for well over 100 students on several different instruments. Not only that, but 
-                    most band directors only receive minimal training in percussion, typically relegated to a single class 
-                    that lasts for only one college semester. You might be fortunate enough to have a percussion instructor 
-                    that comes to your school, but even in that situation you will most likely only see them for anywhere from 
-                    one to several hours per week in which their attention has to be divided among all percussionists in the 
-                    class.
-                </p>
-                <p>
-                    With private instruction, you are guaranteed dedicated one-on-one time each week that will be spent working 
-                    on your specific goals, and improving upon your strengths and weaknesses to make you a better player. 
+                    Our drum lessons cost $25 per half hour lesson. You can pay weekly, or by the month. Lessons are held weekly 
+                    at the same time. Hour lesson slots are also available for $50 per hour lesson.
                 </p>
             </div>
-            <hr />
             <div className='question'>
-                <h2>How should I choose a private instructor?</h2>
+                <h2>What do I need to take lessons?</h2>
             </div>
             <div className='answer'>
                 <p>
-                    Choosing the right private instructor is an important decision and one that shouldn't be taken lightly.
-                </p>
-                <p> 
-                    <strong>Choose a private instructor who has 'walked the walk' and not just 'talked the talk'</strong>, 
-                    so to speak. The goal of learning an instrument is to perform with other musicians on stage, so you should choose 
-                    an instructor who has extensive live performance experience and not someone who has spent most of their time playing 
-                    in the garage with their friends or playing along to recorded music at home.
-                </p>
-                <p>
-                    <strong>They should be well-rounded</strong> in both technique and styles. It should be obvious that you will want 
-                    to choose a teacher who is knowledgeable in your preferred style of music, but you should also choose a teacher that 
-                    knows many different styles and techniques. Your goal should be to become a well-rounded musician, especially if you 
-                    plan on being a working musician one day. You teacher should work to broaden your horizons and introduce you to styles 
-                    of music that you had never even considered before. Also, learning other styles of music can help you excel at your 
-                    chosen style of music. I wouldn't be quite as good of a rock drummer if I didn't know what I know about Latin and 
-                    Afro-Cuban drumming. And I wouldn't be nearly as good at country music without knowing what I know about funt and 
-                    jazz drumming.
-                </p>
-                <p>
-                    <strong>They should be a strong reader</strong>, and they should teach you how to read music. Most of these points 
-                    are non-negotiable, but that goes double for this one. Learning to read music is an <em>absolutely essential </em> 
-                    skill for all musicians, and especially for drummers and percussionists! That being said, the degree to which you should 
-                    learn to read will vary depending upon your personal goals. If you are planning on studying music in college or 
-                    your high school marching band or concert band, being able to read at a high level is extremely important. However, 
-                    if your goal is to play with a touring rock or country band, being able to read at an intermediate level will probably 
-                    be sufficient.
+                    For beginner lessons, all you need is sticks, books, and a practice pad. These items can be purchased from 
+                    any music retailer and are relatively inexpensive. For students taking drum set lessons, they will need a 
+                    drumset at home to practice on. Thankfully, there are many options for drumsets that are also relatively 
+                    inexpensive.
                 </p>
             </div>
-            <hr />
             <div className='question'>
-                <h2>Why should I take lessons at Blevins Percussion?</h2>
+                <h2>Do I have to sign a contract?</h2>
             </div>
             <div className='answer'>
                 <p>
-                    First and foremost, I meet all of the qualifications listed above. I have performed professionally in a variety of 
-                    different styles of music, including with the Chattanooga Symphony as well as several touring rock and country bands 
-                    througout the southeastern United States. From 2010-2022 I worked as a full-time drum/percussion instructor and touring 
-                    musician based in Birmingham, Alabama. Not only that, but I have helped hundreds of students reach their personal goals 
-                    of playing in local bands, achieving spots in school honor bands, attaining music scholarships, and I have even had several 
-                    students go on to pursue careers as professional musicians and educators.
-                </p>
-                <p>
-                    Second, I do not teach drum lessons on electronic drum sets. One of the primary aspects of my lessons that helps my students 
-                    excel is my attention to technique, and one of the most important aspects of any instrument is how to play the instrument with 
-                    proper technique. The problem with electronic drumsets is that they sound the same no matter how you hit them. By only teaching 
-                    lessons on acoustic drumsets, I work on training my students' ears to recognize when they are using poor technique and to be 
-                    able to fix their technique in order to get a better sound. That is virtually impossible to do on an electronic drumset. If you 
-                    are only playing lessons on an electronic drumset, you are only receiving half of an education.
-                </p>
-                <p>
-                    I have nearly unlimited patience. I understand that not every student moves at the same pace, and some students need 
-                    a little more time and attention than others. That's one of the great things about private lessons: we can move as 
-                    quickly or as slowly as you require. One of the compliments that I receive most from my students and they parents is that 
-                    I am always calm and patient, and that my patience is infectious. I actively fight feelings of frustration, because frustration 
-                    is the enemy of progress.
-                </p>
-                <p>
-
+                    No contract! Upon signing up for lessons, I will ask that you sign a list of policies for cancellations, make-up 
+                    lessons, etc., but you are free to end your lessons at any time.
                 </p>
             </div>
+            <div className='question'>
+                <h2>Do you have to be a certain age for drum lessons?</h2>
+            </div>
+                <div className='answer'>
+                <p>
+                    I typically start beginners as young as six years old, but I have had students as young as 4. The oldest student that 
+                    I've had was a 65 year old beginner.
+                </p>
+                </div>
             </div>
         </div>
     )
--- src/components/Lessons.jsx
+++ src/components/Lessons.jsx
@@ -75,11 +75,6 @@ function Lessons () {
             <div>
                 <img className='inline-image' src={lecture2} alt='adam lecturing students at chattanooga high school center for creative arts'></img>
             </div>
-            <div>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/Zq6Nr6CpD4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/sFFSgVe4Dlw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ixPXLzff4U4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
         </div>
     )
 }
